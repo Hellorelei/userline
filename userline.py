1281ffd0d6ba20f8a85d2afbe672d1d9889c8c6d
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2023-12-04 20:39:42 +0100

    initial commit!

--- userline.py
+++ userline.py
@@ -8,13 +8,14 @@ Scripts take in a 'username' and return an array of strings made of [0] a
 value to be display and [1] its formatting (will most likely be colors).
 '''
 import time
+from math import ceil
 from pathlib import Path
 
 INPUT_MODE = 'cli' # Sets username input mode to either 'cli' or 'card'
 VERSION = '0.1'
 MODULE_PATH = Path(__file__).parent / 'modules'
 
-class bcolors:
+class ansi:
     HEADER = '\033[95m'
     BR_MAGENTA = '\033[95m'
     BR_BLUE = '\033[94m'
@@ -25,11 +26,15 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     DIM = '\033[2m'
+    BLINK = '\033[5m'
     UNDERLINE = '\033[4m'
+    RIGHT = '\033[C'
     #FRAMED = '\033[52m'
-    CLEAR = '\033[2K'
-    UP = '\033[2K'
-    TEST = '\033[F'
+    CLEAR = '\033[J'
+    CLINE = '\033[2K'
+    UP = '\033[F'
+    DOWN = '\033[E'
+    ALLUP = '\033[H'
     B_GREEN = '\033[42m'
     B_RED = '\033[41m'
     B_BLUE = '\033[44m'
@@ -43,39 +48,107 @@ class bcolors:
 
 def main():
     '''Main script'''
+    modules = init()
+    while 1:
+        input('  Press any key to enter new username\n')
+        check(modules)
+
+def init():
     printLogo()
-    print(bcolors.BOLD + 'Initializing userline ' + VERSION + bcolors.ENDC)
+    progressBar(0, 0, 12, 0)
+    time.sleep(.3)
+    print(ansi.BOLD + 'Initializing userline ' + VERSION + ansi.ENDC)
+    time.sleep(.3)
+    progressBar(2, 0, 12, 2)
     modules = getModules()
-    username = getUsername()
-    print(bcolors.BOLD + 'Username : ' + bcolors.BR_GREEN + username + bcolors.ENDC)
-    print('')
+
+    i = 0
+    while i < 6:
+        i += 0.4
+        progressBar(6+i, 0, 12, 1)
+        time.sleep(.05)
+
+    print(ansi.UP + ansi.CLINE)
+    return modules
+
 
 def getModules():
-    print(' Looking for modules...')
-    progressBar()
-    return 5
+    print('  Looking for modules...')
+    module_list = []
+    time.sleep(.2)
+    print('    . modules found')
+    x = 0
+    for path in MODULE_PATH.iterdir():
+        module_list.append(path)
+        x += 1
+        print(ansi.UP*x + ansi.CLINE + '    ' + str(x) + ' modules found' + ansi.DOWN*(x-1))
+        print('      ❭ ' + str(path.name))
+        progressBar(2, 2, 12, x+4)
+        time.sleep(.2)
+    progressBar(4, 0, 12, x+4)
+
+    print(ansi.UP*(x+2) +  '  Importing modules...')
+    print('    . modules imported')
+    x = 0
+    for module in module_list:
+        x += 1
+        print(ansi.UP*x + ansi.CLINE + '    ' + str(x) + ' modules imported' + ansi.DOWN*(x-1))
+        print('      ❭ ' + ansi.B_GREEN + str(module.name) + ansi.ENDC)
+        progressBar(4, 2, 12, x+4)
+        time.sleep(.2)
+    progressBar(6, 0, 12, x+4)
+    print((ansi.UP + ansi.CLINE) * (x+4))
+    return module_list
 
 def getUsername():
     '''Obtains username and returns it using 'INPUT_MODE' method'''
     if INPUT_MODE == 'cli':     
-        print(bcolors.BOLD + 'Enter username: ' + bcolors.ENDC)
-        return input()
+        print((ansi.UP + ansi.CLINE)*2 + ansi.BOLD + '  Enter username: ' + ansi.UP + ansi.ENDC)
+        return input(ansi.RIGHT*18)
 
-def progressBar():
-    i = 0
-    print(bcolors.DIM + '   >' + '»' * 40 + '<' + bcolors.ENDC)
-    while i < 40:
-        i += 1
-        print(bcolors.TEST + bcolors.B_MAGENTA + '  (' + '»' * i + bcolors.ENDC)
-        time.sleep(.1)
+def check(x):
+    username = getUsername()
+    print(ansi.UP + ansi.CLINE + ansi.BOLD + '  Username : ' + ansi.BR_GREEN + username + ansi.ENDC)
+    print(ansi.UP)
+    progressBar(1, 0, len(x)+1, 2)
+    j = 0
+    for i in x:
+        progressBar(j+1, 1, len(x)+1, 2+j)
+        time.sleep(1)
+        print('    ok!')
+        progressBar(j+2, 0, len(x)+1, 3+j)
+        j += 1
+    print((ansi.UP + ansi.CLEAR) * (j+2))
+
+def progressBar(step, h_step, steps, dis):
+    x = 40 / steps
+    y = ceil(x*step)
+    hy = ceil(x*h_step)
+    if y > 40:
+        y = 40
+    elif hy > 40:
+        hy = 40
+    elif hy + y > 40:
+        hy = hy-1
+    
+    print(
+        ansi.UP * dis + ansi.CLINE + ansi.ENDC 
+        + '❬' + ('▬' * y) 
+        + ansi.BLINK + ('▰' * hy) + ansi.ENDC 
+        + ansi.DIM + ('▬' * (40-(y+hy))) + ansi.ENDC 
+        + '❭' + ansi.DOWN * (dis-1)
+        )
 
 def printLogo():
-    print(bcolors.BOLD + r'''
-                   ___         
- __ _____ ___ ____/ (_)__  ___ 
-/ // (_-</ -_) __/ / / _ \/ -_)
-\_,_/___/\__/_/ /_/_/_//_/\__/ 
-          ''')
+    print(
+        ansi.BOLD + '─' * 78 + '\n' * 2 + r'''
+                       ___        
+     __ _____ ___ ____/ (_)__  ___ 
+    / // (_-</ -_) __/ / / _ \/ -_)
+    \_,_/___/\__/_/ /_/_/_//_/\__/ ''' + VERSION + '\n'
+        + '\n' * 16 + ansi.UP * 16 + ansi.ENDC
+        )
+    time.sleep(.2)
 
 # End-of-file (EOF)
 if __name__ == "__main__":
